9f0ac7d69903eb7f0508986d912ef2d1b773bb9f
chamit6450 <chamit6450@gmail.com>
2025-04-02 09:24:12 +0530

    txn  logic done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,136 @@
-import { useState } from 'react';
-import './App.css'
+import { useState } from "react";
+import { Connection, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from '@solana/web3.js'
+import "./App.css";
+import axios from "axios";
+
+const connection = new Connection("https://solana-devnet.g.alchemy.com/v2/ZWwUXCzMoNJqmu20g8F9rE6yD90laxIx")
+const senderPubKey = new PublicKey("Ce4JubsvSZXsdU7YQXYgy1jX3WUEiAHTvvWE7G31qkrj")
 
 function App() {
-  const [username, setUsername] = useState();
-  const [password,setPassword] = useState();
+  const [username, setUsername] = useState(""); 
+  const [password, setPassword] = useState("");
+  const [signed, setSigned] = useState(false);
+  const [userpubKey, setPubKey] = useState("");
 
-  async function postData(event){
+  async function postData(event) {
     event.preventDefault();
-     const response = await fetch('http://localhost:3000/api/v1/signup', {
+    const response = await fetch("http://localhost:3000/api/v1/signup", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        username: username,
-        password: password,
-      }),
+      body: JSON.stringify({ username, password }),
     });
 
     const data = await response.json();
-    alert(JSON.stringify(data)  );
-    console.log(data);
+    // alert(JSON.stringify(data));
 
+    setPubKey(data.publicKey);
+
+    console.log("Signup Response:", data);
+
+
+    if (response.ok) {
+      setSigned(true);
+    }
   }
+
+  // async function getData(event) {
+  //   event.preventDefault();
+  //   if (!signed) return;
+
+  //   const response = await fetch(
+  //     `http://localhost:3000/api/v1/signin?username=${username}&password=${password}`,
+  //     {
+  //       method: "GET",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     }
+  //   );
+
+  //   const data = await response.json();
+  //   alert(JSON.stringify(data));
+  // };
+
+  async function sendSol(){
+    // alert("i m sol")
+    console.log("User Public Key:", userpubKey);
+
+    // const senderPubKey = new PublicKey(userpubKey);
+    console.log("User Public Key:", userpubKey);
+    console.log("senderPubKey:", senderPubKey);
+
+
+    const tx = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: senderPubKey,
+        toPubkey: "3hTk6yxkMR4QLbBaFDykKiXkD8zY5rbLNhGPhzJtG1Wv",
+        lamports: 0.01 * LAMPORTS_PER_SOL
+      })
+    )
+   
+    const {blockhash} =await connection.getLatestBlockhash();
+    tx.recentBlockhash = blockhash;
+    tx.feePayer = senderPubKey
+
+    const serializedTxn = tx.serialize({
+      requireAllSignatures : false,
+      verifySignatures : false
+    })
+
+    console.log(serializedTxn);
+    const encodedTxn = serializedTxn.toString("base64");
+
+    const response = await axios.post("http://localhost:3000/api/v1/txn/sign", {
+      serializedtxn: encodedTxn
+    });
+    alert(response.data.signature);
+    console.log("sign successful fe")
+ 
+  };
 
   return (
     <>
-    <form onSubmit={postData}>
-      <input onChange={((event)=>setUsername(event.target.value))} placeholder='Enter username'/>
-      <input onChange={((event)=>setPassword(event.target.value))} placeholder='Enter password'/>
-      <button type='submit'>Submit</button>
-    </form>
+      {/* {!signed ? ( */}
+        <form onSubmit={postData}>
+          <input
+            type="text"
+            onChange={(event) => setUsername(event.target.value)}
+            placeholder="Enter username"
+            value={username}
+          />
+          <input
+            type="password"
+            onChange={(event) => setPassword(event.target.value)}
+            placeholder="Enter password"
+            value={password}
+          />
+          <button type="submit">Sign Up</button>
+        </form>
+        <button onClick={sendSol}>Send Sol</button>
+      {/* // ) : (
+      //   <>
+      //     <form onSubmit={getData}>
+      //       <input
+      //         type="text"
+      //         onChange={(event) => setUsername(event.target.value)}
+      //         placeholder="Enter username"
+      //         value={username}
+      //       />
+      //       <input
+      //         type="password"
+      //         onChange={(event) => setPassword(event.target.value)}
+      //         placeholder="Enter password"
+      //         value={password}
+      //       />
+      //       <button type="submit">Sign In</button>
+      //     </form>
+      //     <button onClick={() => setSigned(false)}>Log out</button>
+      //   </>
+      // )} */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
-const {Keypair} = require('@solana/web3.js');
+const {Keypair, Transaction, Connection} = require('@solana/web3.js');
 const { userModel } = require("./models.js");
 const cors = require('cors');
 const jwt = require("jsonwebtoken");
+// const { connection } = require('mongoose');
 const app = express()
 app.use(express.json())
 app.use(cors())
 const JWT_SECRET = "123456"
 PORT = 3000
+const bs58 = require("bs58");
+
+const connection = new Connection("https://solana-devnet.g.alchemy.com/v2/ZWwUXCzMoNJqmu20g8F9rE6yD90laxIx")
 
 
 app.post("/api/v1/signup", async (req, res) => {
@@ -22,35 +26,65 @@ app.post("/api/v1/signup", async (req, res) => {
         privateKey: keypair.secretKey.toString()
     })
     res.json({
-        message: keypair.privateKey.toString()
-
+        message: keypair.publicKey.toString()
     })
 })
 
-app.post("/api/v1/signin", async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+// app.post("/api/v1/signin", async (req, res) => {
+//     const username = req.body.username;
+//     const password = req.body.password;
 
-    const user = await userModel.findOne({
-        username: username,
-        password: password
-    })
+//     const user = await userModel.findOne({
+//         username: username,
+//         password: password
+//     })
 
-    if (user) {
-        const token = jwt.sign({
-            id: user
-        }, JWT_SECRET)
-        res.json({
-            token
-        })
-    } else {
-        res.status(403).json({
-            message: "Credentials are incorrect"
-        })
+//     if (user) {
+//         const token = jwt.sign({
+//             id: user
+//         }, JWT_SECRET)
+//         res.json({
+//             token
+//         })
+//     } else {
+//         res.status(403).json({
+//             message: "Credentials are incorrect"
+//         })
+//     }
+// })
+
+app.post("/api/v1/txn/sign", async (req, res) => {
+ 
+    
+    const serializedtxn = req.body.serializedtxn;
+    if (!serializedtxn) {
+        return res.status(400).json({ error: "Missing transaction data" });
     }
+    console.log("check 1")
+
+const tx = Transaction.from(Buffer.from(serializedtxn, "base64")); // Decode base64
+
+
+  console.log(tx);
+  console.log("check 2")
+  const keyPair = Keypair.fromSecretKey(bs58.decode(process.env.PRIVATE_KEY));
+  console.log("check 3")
+
+  const {blockhash} = await connection.getLatestBlockhash();
+  tx.blockhash = blockhash
+  tx.feePayer = keyPair.publicKey
+  console.log("check 4")
+  tx.sign(keyPair);
+
+  const signature = await connection.sendTransaction(tx, [keyPair]);
+  console.log("this ie me" ,signature)
+  
+
+  res.json({
+    message: "Transaction submitted successfully",
+    signature: signature,
+});
 })
-
-
 
 app.listen(PORT,()=>
  console.log(`this app is running on http://localhost:${PORT}`)
